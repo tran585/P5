8d1b0dad2b6f72957ed384260d29c8c0bf745a58
TRAN_Thanh <montblanc670000@gmail.com>
2023-11-15 03:20:20 +0100

    Ajout des bullet points au slider + étape 4 terminée + l'ajout des conditions pour le défilement (étape 5),  reste à faire = vérification code + synthaxe avant validation de l'étape 5

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,69 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+	},
 ]
+
+let container_banner = document.querySelectorAll("#banner p, span")
+let arrow_left_right = document.querySelectorAll("#banner .arrow")
+let test = document.querySelector("#banner .banner-img")
+let bullet = document.querySelectorAll("#banner .dots .dot")
+let i = 0
+let d = 0
+bullet[i].classList.add("dot_selected")
+
+
+//left
+arrow_left_right[0].addEventListener("click", function() {
+		d = +1
+		i--
+		d += i //pour l'itération du bullet right
+		if (i < 0) {
+			i = 3
+			d = 0
+			console.log(i)
+			console.log(d)
+		}
+		console.log("arrow_left")
+		test.src = slides[i].image
+		container_banner[0].innerHTML = slides[i].tagLine
+		bullet[i].classList.add("dot_selected")
+		bullet[d].classList.remove("dot_selected")
+		console.log(i)
+		console.log(d)
+	}
+)
+
+//right
+arrow_left_right[1].addEventListener("click", function() {
+		d = -1
+		i++ 
+		d += i //pour l'itération du bullet right
+		if (i > 3) {
+			d = 3
+			i = 0
+			console.log(i)
+			console.log(d)
+		}
+		console.log("arrow_right")
+		test.src = slides[i].image
+		container_banner[0].innerHTML = slides[i].tagLine
+		bullet[i].classList.add("dot_selected")
+		bullet[d].classList.remove("dot_selected")
+		console.log(i)
+		console.log(d)
+	}
+)
